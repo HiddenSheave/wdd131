13ec2f05ed73c8d9e53f3a1f5ce7262809536f7d
HiddenSheave <brewers.sheave.05@icloud.com>
2024-11-02 18:37:25 +0100

    center footer

--- week01/scripts/calculator.js
+++ week01/scripts/calculator.js
@@ -2,9 +2,8 @@ let equalButton = document.getElementById("doTheCalculation");
 
 equalButton.addEventListener("click", () => {
     // Get the number 1 value
-let number1 = document.getElementById("number1");
-let number2 = document.getElementById("number2");
-let sum = addNumbers("number1, number2");
-console.log("Result" number1 + "and" + number2 "is" + sum);
-
+const number1 = document.getElementById("number1").value;
+const number2 = document.getElementById("number2");
+const sum = addNumbers("number1, number2");
+document.getElementById("result").textContent = `Result: ${sum}`;
 });
